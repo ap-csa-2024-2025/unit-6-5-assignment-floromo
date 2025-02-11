93a8a4cb38b098a8e3f3c00ad947b309394ab1e2
floromo <145050916+floromo@users.noreply.github.com>
2025-02-11 18:15:27 +0000

    donezo

--- Main.java
+++ Main.java
@@ -12,25 +12,61 @@ public class Main
     sc.nextLine();
 
     System.out.println("Enter values: ");
-    String arr1 = new String[len];
+    String[] arr1 = new String[len];
     for (int i = 0; i < len; i++){
       arr1[i] = sc.nextLine();
     }
+    System.out.println("Reversed:");
     reverse(arr1);
+
+    // Problem 2
+    System.out.println("Enter array length: ");
+    len = sc.nextInt();
+
+    System.out.println("Enter values: ");
+    int[] arr2 = new int[len];
+    for (int i = 0; i < len; i++){
+      arr2[i] = sc.nextInt();
+    }
+    System.out.println("Product is: " + product(arr2));
+
+    // Problem 3
+    System.out.println("Enter array length: ");
+    len = sc.nextInt();
+
+    System.out.println("Enter values: ");
+    double[] arr3 = new double[len];
+    for (int i = 0; i < len; i++){
+      arr3[i] = sc.nextDouble();
+    }
+    System.out.println("Average is: " + average(arr3));
   }
 
   public static void reverse(String[] arr)
   {
-    System.out.println(" ");
+    for (String word : arr){
+      for (int j = word.length(); j > 0; j--){
+        System.out.print(word.substring(j-1, j));
+      }
+      System.out.println("");
+    }
   }
 
-  public int product(int[] arr)
+  public static int product(int[] arr)
   {
-    return 0;
+    int num = 1;
+    for (int i : arr){
+      num *= i;
+    }
+    return num;
   }
 
-  public double average(double[] arr)
+  public static double average(double[] arr)
   {
-    return 0.0;
+    double avg = 0;
+    for (double i : arr){
+      avg += i;
+    }
+    return avg/arr.length;
   }
 }
